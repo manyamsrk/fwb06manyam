27188b70732a7e594601ce2f97979f820c9c2e5e
Siva Rama Krishna Manyam <S544906@nwmissouri.edu>
2021-10-30 12:58:31 -0500

    addmods

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var gasRouter = require('./routes/gas',)
+var gasRouter = require('./routes/gas');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gas',gasRouter);
+app.use('/addmods',addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
